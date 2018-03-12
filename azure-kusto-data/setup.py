7bf1fc54708ba46a7d497a47a1b80a3af05c1b83
Tomer Shetah <toshetah@microsoft.com>
2018-03-12 21:47:23 +0200

    Add support at puthon 3

--- azure-kusto-data/setup.py
+++ azure-kusto-data/setup.py
@@ -46,6 +46,7 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
