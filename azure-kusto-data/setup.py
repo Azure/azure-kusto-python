52be7cbbb6597cc207b8b26b4d8247460d88dd30
Tomer Shetah <toshetah@microsoft.com>
2018-03-12 21:31:15 +0200

    Remove support at puthon 3

--- azure-kusto-data/setup.py
+++ azure-kusto-data/setup.py
@@ -46,7 +46,6 @@ setup(
         'Intended Audience :: Developers',
         'Topic :: Software Development',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
