e35784029fff7179b53bb6c1340a44b40c3584db
Tomer Shetah <toshetah@microsoft.com>
2018-03-14 18:56:33 +0200

    Do not support python 3 at the moment.

--- azure-kusto-ingest/setup.py
+++ azure-kusto-ingest/setup.py
@@ -36,7 +36,6 @@ setup(
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
