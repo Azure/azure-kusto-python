bc7c66866fbc9b1c59938f292df6ceb6f9e64278
Tomer Shetah <toshetah@microsoft.com>
2018-03-15 16:57:33 +0200

    Add build_packages

---
+++ build_packages.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+from subprocess import check_call
+
+DEFAULT_DESTINATION_FOLDER = "../dist"
+package_list = ['azure-kusto-data', 'azure-kusto-ingest']
+
+
+def create_package(name, dest_folder=DEFAULT_DESTINATION_FOLDER):
+    absdirpath = os.path.abspath(name)
+    check_call(['python', 'setup.py', 'bdist_wheel', '-d', dest_folder], cwd=absdirpath)
+    check_call(['python', 'setup.py', "sdist", '-d', dest_folder], cwd=absdirpath)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Build Azure package.')
+    parser.add_argument('name', help='The package name')
+    parser.add_argument('--dest', '-d', default=DEFAULT_DESTINATION_FOLDER,
+                        help='Destination folder. Relative to the package dir. [default: %(default)s]')
+
+    args = parser.parse_args()
+    if args.name == 'all':
+        for package in package_list:
+            create_package(package, args.dest)
+    else:
+        create_package(args.name, args.dest)
