04376c8e679cf9393e72947d0de4e0088740de32
Tomer Shetah <toshetah@microsoft.com>
2018-05-17 17:05:51 +0300

    Bug fix

--- azure-kusto-data/azure/kusto/data/kusto_client.py
+++ azure-kusto-data/azure/kusto/data/kusto_client.py
@@ -162,7 +162,7 @@ class KustoResponse(object):
             col_name = col["ColumnName"]
             col_type = col['ColumnType'] if 'ColumnType' in col else col['DataType']
             if col_type.lower() == "timespan":
-                frame[col_name] = pandas.to_timedelta(frame[col_name].apply(lambda t: t.replace(".", " days ") if '.' in t else t))
+                frame[col_name] = pandas.to_timedelta(frame[col_name].apply(lambda t: t.replace(".", " days ") if t and '.' in t.split(":")[0] else t))
             elif col_type.lower() == "dynamic":
                 frame[col_name] = frame[col_name].apply(lambda x: json.loads(x) if x else None)
             else:
