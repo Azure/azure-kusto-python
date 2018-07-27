60793305c2be3164bd4863761692cc8f93e7ee12
Tomer Shetah <toshetah@microsoft.com>
2018-07-27 09:53:04 +0300

    fix sample

--- azure-kusto-ingest/tests/sample.py
+++ azure-kusto-ingest/tests/sample.py
@@ -2,7 +2,7 @@ from azure.kusto.ingest import KustoIngestClient, IngestionProperties, FileDescr
 
 ingestion_properties = IngestionProperties(database="database name", table="table name", dataFormat=DataFormat.csv)
 
-ingest_client = KustoIngestClient("https://ingest-clustername.kusto.windows.net", username="username@microsoft.com")
+ingest_client = KustoIngestClient("https://ingest-clustername.kusto.windows.net")
 ingest_client = KustoIngestClient("https://ingest-clustername.kusto.windows.net", client_id="aad app id", client_secret="secret")
 
 file_descriptor = FileDescriptor("E:\\filePath.csv", 3333) # 3333 is the raw size of the data in bytes.
@@ -15,6 +15,6 @@ ingest_client.ingest_from_multiple_files(["E:\\filePath.csv"],
                                           ingestion_properties=ingestion_properties)
 
 blob_descriptor = BlobDescriptor("https://path-to-blob.csv.gz?sas", 10) # 10 is the raw size of the data in bytes.
-ingest_client.ingest_from_multiple_files([blob_descriptor],
+ingest_client.ingest_from_multiple_blobs([blob_descriptor],
                                           delete_sources_on_success=True,
                                           ingestion_properties=ingestion_properties)
