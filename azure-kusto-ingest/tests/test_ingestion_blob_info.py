aebe09da8745b14da6b44a60e1cdd87ed9edc789
Tomer Shetah <toshetah@microsoft.com>
2018-08-08 16:25:56 +0300

    another exception use

--- azure-kusto-ingest/tests/test_ingestion_blob_info.py
+++ azure-kusto-ingest/tests/test_ingestion_blob_info.py
@@ -5,8 +5,8 @@ import re
 import json
 from uuid import UUID
 from azure.kusto.ingest.ingestion_blob_info import _IngestionBlobInfo
+from azure.kusto.ingest.exceptions import KustoDuplicateMappingError
 from azure.kusto.ingest import (
-    KustoDuplicateMappingError,
     BlobDescriptor,
     IngestionProperties,
     DataFormat,
