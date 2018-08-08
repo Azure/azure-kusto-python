ec8f0b075e12e1dfdd57975edc65d8f1559dc549
Tomer Shetah <toshetah@microsoft.com>
2018-08-08 17:32:52 +0300

    Edit init file

--- azure-kusto-ingest/azure/kusto/ingest/__init__.py
+++ azure-kusto-ingest/azure/kusto/ingest/__init__.py
@@ -1,4 +1,4 @@
-from .kusto_ingest_client import KustoIngestClient
+from ._kusto_ingest_client import KustoIngestClient
 from ._descriptors import BlobDescriptor, FileDescriptor
 from ._ingestion_properties import (
     DataFormat,
