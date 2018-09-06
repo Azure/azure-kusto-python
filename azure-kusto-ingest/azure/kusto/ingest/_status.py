4a4e5b0a735fc92ce4d1213c3ce7a56f95048c4e
Daniel Dubovski <dadubovs@microsoft.com>
2018-09-06 17:38:00 +0300

    renaming for readability

--- azure-kusto-ingest/azure/kusto/ingest/_status.py
+++ azure-kusto-ingest/azure/kusto/ingest/_status.py
@@ -1,88 +1,66 @@
-from azure.storage.common import CloudStorageAccount
+"""Kusto ingest client for Python."""
+
+import json
 import base64
 
+import six
+from azure.storage.common import CloudStorageAccount
+from ._status_q import StatusQueue
 
-class QueueDetails(object):
-    def __init__(self, name, service):
-        self.name = name
-        self.service = service
+
+class StatusMessage(object):
+    OperationId = None
+    Database = None
+    Table = None
+    IngestionSourceId = None
+    IngestionSourcePath = None
+    RootActivityId = None
+
+    _raw = None
+
+    def __init__(self, str):
+        self._raw = str
+
+        o = json.loads(str)
+        for key, value in six.iteritems(o):
+            if hasattr(self, key):
+                try:
+                    setattr(self, key, value)
+                except:
+                    # TODO: should we set up a logger?
+                    pass
 
     def __str__(self):
-        return "QueueDetails({0.name})".format(self)
+        return "{}".format(self._raw)
+
+    def __repr__(self):
+        return "{0.__class__.__name__}({0._raw})".format(self)
 
 
-class StatusQueue(object):
-    """StatusQueue is a class to simplify access to Kusto status queues (backed by azure storage queues).
+class SuccessMessage(StatusMessage):
+    SucceededOn = None
+
+
+class FailureMessage(StatusMessage):
+    FailedOn = None
+    Details = None
+    ErrorCode = None
+    FailureStatus = None
+    OriginatesFromUpdatePolicy = None
+    ShouldRetry = None
+
+
+class KustoIngestStatusQueues(object):
+    """Kusto ingest Status Queue.
+    Use this class to get status messages from Kusto status queues.
+    Currently there are two queues exposed: `failure` and `success` queues.
     """
-    def __init__(self, get_queues_func, message_cls):
-        self.get_queues_func = get_queues_func
-        self.message_cls = message_cls
 
-    def _get_q_services(self, queues_details):
-        return [
-            QueueDetails(
-                name=queue_details.object_name,
-                service=CloudStorageAccount(
-                    account_name=queue_details.storage_account_name, sas_token=queue_details.sas
-                ).create_queue_service(),
-            )
-            for queue_details in queues_details
-        ]
+    def __init__(self, kusto_ingest_client):
+        self.success = StatusQueue(
+            kusto_ingest_client._resource_manager.get_successful_ingestions_queues, message_cls=SuccessMessage
+        )
+        self.failure = StatusQueue(
+            kusto_ingest_client._resource_manager.get_failed_ingestions_queues, message_cls=FailureMessage
+        )
 
-    def is_empty(self):
-        """Checks if Status queue has any messages        
-        """
-        return self.peek(1, raw=True)
-
-    def decode_content(self, content):
-        return base64.b64decode(content)
-
-    def deserialize_message(self, m):
-        """Deserialize a message and return at as `message_cls`
-        :param m: original message m.
-        """
-        return self.message_cls(self.decode_content(m.content))
-
-
-    # TODO: current implemtation takes a union top n /  len(queues), which is not ideal,
-    # becuase the user is not supposed to know that there can be multiple underlying queues
-    def peek(self, n, raw=False):
-        """Peek at status queue
-        :param int n: number of messages to return as part of peek.
-        :param bool raw: should message content be returned as is (no parsing).
-        """
-        q_services = self._get_q_services(self.get_queues_func())
-
-        per_q = int(n / len(q_services)) + 1
-
-        result = []
-        for q in q_services:
-            for m in q.service.peek_messages(q.name, num_messages=per_q):
-                if m is not None:
-                    result.append(m if raw else self.deserialize_message(m))
-
-        # because we ask for n / len(qs) + 1, we might get more message then requests
-        return result[:n]
-
-    # TODO: current implemtation takes a union top n /  len(queues), which is not ideal,
-    # becuase the user is not supposed to know that there can be multiple underlying queues
-    def pop(self, n, raw=False, delete=True):
-        """Pop status queue
-        :param int n: number of messages to return as part of peek.
-        :param bool raw: should message content be returned as is (no parsing).
-        :param bool delete: should message be deleted after pop. default is True as this is expected of a q.
-        """
-        q_services = self._get_q_services(self.get_queues_func())
-
-        per_q = int(n / len(q_services)) + 1
-
-        result = []
-        for q in q_services:
-            for m in q.service.get_messages(q.name, num_messages=per_q):
-                if m is not None:
-                    result.append(m if raw else self.deserialize_message(m))
-                    if delete:
-                        q.service.delete_message(q.name, m.id, m.pop_receipt)
-
-        # because we ask for n / len(qs) + 1, we might get more message then requests
-        return result[:n]
