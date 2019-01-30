33942c15ab57f09bbdeba61d31bd5c0692cfc00f
Tomer Shetah <toshetah@microsoft.com>
2019-01-30 17:47:53 +0200

    Stop supporting python 3.4

--- azure-kusto-ingest/setup.py
+++ azure-kusto-ingest/setup.py
@@ -31,7 +31,6 @@ setup(
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: MIT License",
