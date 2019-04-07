b8b30151f0c63e01ecc5367e6db55474ff1012bf
Ariel Yehezkely <37837385+ArielYehezkely@users.noreply.github.com>
2019-04-07 10:50:34 +0300

    Add streaming ingest examples in sample

--- azure-kusto-ingest/azure/kusto/ingest/__init__.py
+++ azure-kusto-ingest/azure/kusto/ingest/__init__.py
@@ -2,7 +2,7 @@
 
 from ._ingest_client import KustoIngestClient
 from ._streaming_ingest_client import KustoStreamingIngestClient
-from ._descriptors import BlobDescriptor, FileDescriptor
+from ._descriptors import BlobDescriptor, FileDescriptor, StreamDescriptor
 from ._ingestion_properties import (
     DataFormat,
     ValidationPolicy,
