260bcc859c926f2af7b1245becca45d821522bf2
Luca Boccassi <luca.boccassi@microsoft.com>
2020-05-14 18:37:46 +0100

    Restore compatibility with dateutil 2.7.x
    Many distributions are using dateutil 2.7.x, including Debian stable.
    Restore compatibility by truncating timestamps with a precision of
    more than a microsecond.

--- azure-kusto-data/azure/kusto/data/_converters.py
+++ azure-kusto-data/azure/kusto/data/_converters.py
@@ -7,13 +7,21 @@ from dateutil import parser
 
 # Regex for TimeSpan
 _TIMESPAN_PATTERN = re.compile(r"(-?)((?P<d>[0-9]*).)?(?P<h>[0-9]{2}):(?P<m>[0-9]{2}):(?P<s>[0-9]{2}(\.[0-9]+)?$)")
+_MICROSECONDS_PATTERN = re.compile(r"(.*\.[0-9]{0,6})[0-9]*([A-Z]*)")
 
 
 def to_datetime(value):
     """Converts a string to a datetime."""
     if isinstance(value, int):
         return parser.parse(value)
-    return parser.isoparse(value)
+    # Until v2.8.0 dateutil does not support more than 6 digits for fractional seconds
+    try:
+        return parser.isoparse(value)
+    except ValueError:
+        match = _MICROSECONDS_PATTERN.match(value)
+        if match:
+            value = match.group(1) + match.group(2)
+        return parser.isoparse(value)
 
 
 def to_timedelta(value):
