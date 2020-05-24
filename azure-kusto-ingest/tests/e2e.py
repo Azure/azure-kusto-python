a1333c7e8dd05648644209e85b1d6188290892a1
Daniel Dubovski <>
2020-05-24 15:40:41 +0300

    table name fix

--- azure-kusto-ingest/tests/e2e.py
+++ azure-kusto-ingest/tests/e2e.py
@@ -147,7 +147,7 @@ def get_file_path() -> str:
 # Init clients
 test_db = os.environ.get("TEST_DATABASE")
 
-python_version = ".".join([str(v) for v in sys.version_info[:3]])
+python_version = "_".join([str(v) for v in sys.version_info[:3]])
 test_table = "python_test_{0}_{1}".format(python_version, str(int(time.time())))
 client = KustoClient(engine_kcsb_from_env())
 ingest_client = KustoIngestClient(dm_kcsb_from_env())
