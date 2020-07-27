59aa4edf7001bd7701dd158072783dc638079ccd
Yochai Gilad <yogilad@microsoft.com>
2020-07-27 09:53:18 +0300

    checkpoint: App ID & Key works

--- azure-kusto-ingest/tests/azure/auth_e2e.py
+++ azure-kusto-ingest/tests/azure/auth_e2e.py
@@ -25,37 +25,40 @@ def main():
     # App ID and Key based auth
     app_id = os.environ.get("APP_ID")
     app_key = os.environ.get("APP_KEY")
+    auth_id = os.environ.get("AUTH_ID")
 
     ###############################################################################
+    faults = 0
 
     # Test MSI auth
-    kcsb = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentication(cluster_uri)
-    run_test("Connect with system MSI", lambda: ping_server(kcsb))
+    if user_msi_client_id is not None or user_msi_object_id is not None:
+        kcsb = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentication(cluster_uri)
+        faults += run_test("Connect with system MSI", lambda: ping_server(kcsb))
 
     if user_msi_client_id is not None:
         kcsb = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentication(cluster_uri, client_id=user_msi_client_id)
-        run_test("Connect with client id MSI", lambda: ping_server(kcsb))
+        faults += run_test("Connect with client id MSI", lambda: ping_server(kcsb))
 
     if user_msi_object_id is not None:
         kcsb = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentication(cluster_uri, object_id=user_msi_object_id)
-        run_test("Connect with object id MSI", lambda: ping_server(kcsb))
+        faults += run_test("Connect with object id MSI", lambda: ping_server(kcsb))
 
     # Test App Id and Key auth
     if app_key is not None and app_id is not None:
-        kcsb = KustoConnectionStringBuilder.with_aad_application_key_authentication(cluster_uri, app_id, app_key)
-        run_test("Connect with App Id & Key", lambda: ping_server(kcsb))
+        kcsb = KustoConnectionStringBuilder.with_aad_application_key_authentication(cluster_uri, app_id, app_key, auth_id)
+        faults += run_test("Connect with App Id & Key", lambda: ping_server(kcsb))
 
     # Test Certificate auth
     if app_id is not None and certificate_file is not None and certificate_private_key is not None and certificate_authority is not None:
         kcsb = KustoConnectionStringBuilder.with_aad_application_certificate_authentication(
             cluster_uri, app_id, certificate_private_key, certificate_thumbprint, certificate_authority
         )
-        run_test("Connect with certificate", lambda: ping_server(kcsb))
+        faults += run_test("Connect with certificate", lambda: ping_server(kcsb))
 
         kcsb = KustoConnectionStringBuilder.with_aad_application_certificate_authentication(
             cluster_uri, app_id, certificate_private_key, certificate_thumbprint, certificate_authority, certificate_file
         )
-        run_test("Connect with certificate subject name and issuer", lambda: ping_server(kcsb))
+        faults += run_test("Connect with certificate subject name and issuer", lambda: ping_server(kcsb))
 
     # Test manual options
     token = None
@@ -64,30 +67,37 @@ def main():
         token = token_provider()
 
         kcsb = KustoConnectionStringBuilder.with_token_provider(cluster_uri, token_provider)
-        run_test("Connect with token provider", lambda: ping_server(kcsb))
+        faults += run_test("Connect with token provider", lambda: ping_server(kcsb))
 
         kcsb = KustoConnectionStringBuilder.with_aad_user_token_authentication(cluster_uri, token)
-        run_test("Connect with user provided token", lambda: ping_server(kcsb))
+        faults += run_test("Connect with user provided token", lambda: ping_server(kcsb))
     except Exception as e:
         print("failed to get system MSI token. Skipping token & token-provider tests")
         print(e)
 
     # Test Az Cli
-    az_installed = subprocess.run(["where", "az"], capture_output=True)
-    if az_installed.returncode == 0:
-        print("Trying login with az-cli. This may fail if refresh token is expired")
-        kcsb.with_az_cli_authentication(cluster_uri)
-        run_test("Connect with az-cli", lambda: ping_server(kcsb))
+    try:
+        az_installed = subprocess.run(["where", "az"], capture_output=True)
+        if az_installed.returncode == 0:
+            print("Trying login with az-cli. This may fail if refresh token is expired")
+            kcsb.with_az_cli_authentication(cluster_uri)
+            faults += run_test("Connect with az-cli", lambda: ping_server(kcsb))
+    except Exception:
+        None
+
+    exit(faults)
 
 
-def run_test(name: str, test: callable):
+def run_test(name: str, test: callable) -> int:
     try:
         print("Testing: " + name + "... ", end="")
         test()
         print("Succeeded")
+        return 0
     except Exception as e:
         print("Failed")
         print(e)
+        return 1
 
 
 def ping_server(kcsb: KustoConnectionStringBuilder):
