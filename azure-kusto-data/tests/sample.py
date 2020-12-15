766a51f4b4a46ae7d4da308a2332333c90b31dac
Christian Coello <Christian.e.coello@gmail.com>
2020-12-15 10:34:04 -0500

    Minor punctuation

--- azure-kusto-data/tests/sample.py
+++ azure-kusto-data/tests/sample.py
@@ -53,7 +53,7 @@ user_assigned_client_id = "the AAD identity client id"
 kcsb = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentication(cluster, client_id=user_assigned_client_id)
 
 # In case you want to authenticate with Azure CLI.
-# First, run "az account get-access-token" to cache token locally
+# First, run "az account get-access-token" to cache token locally.
 kcsb = KustoConnectionStringBuilder.with_az_cli_authentication(cluster)
 
 # In case you want to authenticate with AAD username and password
