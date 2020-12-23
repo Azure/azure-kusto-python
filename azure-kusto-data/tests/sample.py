b19bdd98c48463589120f3b3f8eea98797540b24
Christian Coello <Christian.e.coello@gmail.com>
2020-12-23 14:06:44 -0500

    Update azure cli docs (#289)
    * Update sample to include az cli auth
    
    * Remove outdated comment and mention az cli
    
    * Clarify az cli difference with azure-kusto-python
    
    Co-authored-by: Yochai Gilad <yogilad@microsoft.com>
    
    * Update azure-kusto-data/tests/sample.py
    
    Co-authored-by: Yochai Gilad <yogilad@microsoft.com>
    
    Co-authored-by: Yochai Gilad <yogilad@microsoft.com>

--- azure-kusto-data/tests/sample.py
+++ azure-kusto-data/tests/sample.py
@@ -52,6 +52,11 @@ kcsb = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentica
 user_assigned_client_id = "the AAD identity client id"
 kcsb = KustoConnectionStringBuilder.with_aad_managed_service_identity_authentication(cluster, client_id=user_assigned_client_id)
 
+# In case you want to authenticate with Azure CLI.
+# Users are required to be in a logged in state in az-cli, for this authentication method to succeed. Run `az login` to login to azure cli.
+
+kcsb = KustoConnectionStringBuilder.with_az_cli_authentication(cluster)
+
 # In case you want to authenticate with AAD username and password
 username = "<username>"
 password = "<password>"
