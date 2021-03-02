1feee43250f33e1df469fd7ed00fe4ae15ed49d1
Yochai Gilad <yogilad@microsoft.com>
2021-03-02 17:13:03 +0200

    reverting test.bat changes due to problems

--- test.bat
+++ test.bat
@@ -1,8 +1,7 @@
 cd %PROJECTS_HOME%\azure-kusto-python
 call workon kusto
 call pip uninstall azure-kusto-data azure-kusto-ingest -y 
-call pip install -e ./azure-kusto-data[pandas]
-call pip install -e ./azure-kusto-ingest[pandas]
+call pip install ./azure-kusto-data[pandas] ./azure-kusto-ingest[pandas] 
 call pip install --force-reinstall azure-nspkg==2.0.0 
 call pytest
 pause
