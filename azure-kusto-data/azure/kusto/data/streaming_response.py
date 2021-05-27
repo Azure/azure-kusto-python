f0ed98a948eb3ac669b48d6ed8f8ffc06e08de4f
Asaf Mahlev <asafmahlev@microsoft.com>
2021-05-27 16:32:07 +0300

    Don't use JSON magic strings, add official dependency for ijson

--- azure-kusto-data/azure/kusto/data/streaming_response.py
+++ azure-kusto-data/azure/kusto/data/streaming_response.py
@@ -2,15 +2,44 @@ import io
 from enum import Enum
 from typing import Optional, Any, Tuple, Dict
 
-import dataclasses
 import ijson
 
 
-@dataclasses.dataclass
+class JsonTokenType(Enum):
+    NULL = 0
+    BOOLEAN = 1
+    NUMBER = 2
+    STRING = 3
+    MAP_KEY = 4
+    START_MAP = 5
+    END_MAP = 6
+    START_ARRAY = 7
+    END_ARRAY = 8
+
+    @staticmethod
+    def start_tokens():
+        return [JsonTokenType.START_MAP, JsonTokenType.START_ARRAY]
+
+    @staticmethod
+    def end_tokens():
+        return [JsonTokenType.END_MAP, JsonTokenType.END_ARRAY]
+
+
+class FrameType(Enum):
+    DataSetHeader = 0
+    TableHeader = 1
+    TableFragment = 2
+    TableCompletion = 3
+    TableProgress = 4
+    DataTable = 5
+    DataSetCompletion = 6
+
+
 class JsonToken:
-    token_path: str
-    token_type: str
-    token_value: Optional[Any]
+    def __init__(self, token_path: str, token_type: JsonTokenType, token_value: Optional[Any]) -> None:
+        self.token_path = token_path
+        self.token_type = token_type
+        self.token_value = token_value
 
 
 class JsonTokenReader:
@@ -22,46 +51,48 @@ class JsonTokenReader:
         next_item = next(self.json_iter)
         if next_item is None:
             raise Exception("Unexpected end of stream")  # todo - better exception
+        (token_path, token_type, token_value) = next_item
 
-        return JsonToken(*next_item)
+        return JsonToken(token_path, JsonTokenType[token_type.upper()], token_value)
 
-    def read_token_of_type(self, *token_types: str) -> JsonToken:
+    def read_token_of_type(self, *token_types: JsonTokenType) -> JsonToken:
         token = self.read_next_token_or_throw()
         if token.token_type not in token_types:
-            raise Exception(f"Expected one the following types: '{','.join(token_types)}' , got type {token.token_type}")  # todo - better exception
+            raise Exception(
+                f"Expected one the following types: '{','.join(t.name for t in token_types)}' , got type {token.token_type}")  # todo - better exception
         return token
 
     def read_start_object(self) -> JsonToken:
-        return self.read_token_of_type("start_map")
+        return self.read_token_of_type(JsonTokenType.START_MAP)
 
     def read_start_array(self) -> JsonToken:
-        return self.read_token_of_type("start_array")
+        return self.read_token_of_type(JsonTokenType.START_ARRAY)
 
     def read_string(self) -> str:
-        return self.read_token_of_type("string").token_value
+        return self.read_token_of_type(JsonTokenType.STRING).token_value
 
     def read_boolean(self) -> bool:
-        return self.read_token_of_type("boolean").token_value
+        return self.read_token_of_type(JsonTokenType.BOOLEAN).token_value
 
     def read_number(self) -> float:
-        return self.read_token_of_type("number").token_value
+        return self.read_token_of_type(JsonTokenType.NUMBER).token_value
 
     def skip_to_end(self):
         for i in self.json_iter:
             pass
 
-    def skip_children(self, prev_token):
-        if prev_token.token_type == "map_key":
+    def skip_children(self, prev_token: JsonToken):
+        if prev_token.token_type == JsonTokenType.MAP_KEY:
             self.read_next_token_or_throw()
-        elif prev_token.token_type.startswith("start"):
+        elif prev_token.token_type in JsonTokenType.start_tokens():
             while True:
                 potential_end_token = self.read_next_token_or_throw()
-                if potential_end_token.token_path == prev_token.token_path and potential_end_token.token_type.startswith("end"):
+                if potential_end_token.token_path == prev_token.token_path and potential_end_token.token_type in JsonTokenType.end_tokens():
                     break
 
     def skip_until_property_name(self, name: str):
         while True:
-            token = self.read_token_of_type("map_key")
+            token = self.read_token_of_type(JsonTokenType.MAP_KEY)
             if token.token_value == name:
                 return token
 
@@ -70,10 +101,10 @@ class JsonTokenReader:
     def skip_until_property_name_or_end_object(self, *names: str) -> JsonToken:
         while True:
             token = self.read_next_token_or_throw()
-            if token.token_type == "end_map":
+            if token.token_type == JsonTokenType.END_MAP:
                 return token
 
-            if token.token_type == "map_key":
+            if token.token_type == JsonTokenType.MAP_KEY:
                 if token.token_value in names:
                     return token
 
@@ -81,7 +112,7 @@ class JsonTokenReader:
 
             raise Exception(f"Unexpected token {token}")
 
-    def skip_until_token(self, *tokens: str):
+    def skip_until_token(self, *tokens: JsonTokenType):
         while True:
             token = self.read_next_token_or_throw()
             if token.token_type in tokens:
@@ -89,17 +120,7 @@ class JsonTokenReader:
             self.skip_children(token)
 
     def skip_until_end_object(self):
-        self.skip_until_token("end_map")
-
-
-class FrameType(Enum):
-    DataSetHeader = 0
-    TableHeader = 1
-    TableFragment = 2
-    TableCompletion = 3
-    TableProgress = 4
-    DataTable = 5
-    DataSetCompletion = 6
+        self.skip_until_token(JsonTokenType.END_MAP)
 
 
 class ProgressiveDataSetEnumerator:
@@ -115,51 +136,54 @@ class ProgressiveDataSetEnumerator:
         if self.done:
             raise StopIteration()
 
-        token = self.reader.skip_until_token("start_map", "end_array")
-        if token == "end_array":
+        token = self.reader.skip_until_token(JsonTokenType.START_MAP, JsonTokenType.END_ARRAY)
+        if token == JsonTokenType.END_ARRAY:
             self.done = True
             raise StopIteration()
 
         frame_type = self.read_frame_type()
 
         if frame_type == FrameType.DataSetHeader:
-            return self.extract_props(frame_type, ("IsProgressive", "boolean"), ("Version", "string"))
+            return self.extract_props(frame_type, ("IsProgressive", JsonTokenType.BOOLEAN), ("Version", JsonTokenType.STRING))
         elif frame_type == FrameType.TableHeader:
-            return self.extract_props(frame_type, ("TableId", "number"), ("TableKind", "string"), ("TableName", "string"), ("Columns", "array"))
+            return self.extract_props(frame_type, ("TableId", JsonTokenType.NUMBER), ("TableKind", JsonTokenType.STRING), ("TableName", JsonTokenType.STRING),
+                                      ("Columns", JsonTokenType.START_ARRAY))
         elif frame_type == FrameType.TableFragment:
-            return self.extract_props(frame_type, ("TableFragmentType", "string"), ("TableId", "number"), ("Rows", "array"))
+            return self.extract_props(frame_type, ("TableFragmentType", JsonTokenType.STRING), ("TableId", JsonTokenType.NUMBER),
+                                      ("Rows", JsonTokenType.START_ARRAY))
         elif frame_type == FrameType.TableCompletion:
-            return self.extract_props(frame_type, ("TableId", "number"), ("RowCount", "number"))
+            return self.extract_props(frame_type, ("TableId", JsonTokenType.NUMBER), ("RowCount", JsonTokenType.NUMBER))
         elif frame_type == FrameType.TableProgress:
-            return self.extract_props(frame_type, ("TableId", "number"), ("TableProgress", "number"))
+            return self.extract_props(frame_type, ("TableId", JsonTokenType.NUMBER), ("TableProgress", JsonTokenType.NUMBER))
         elif frame_type == FrameType.DataTable:
-            props = self.extract_props(frame_type, ("TableId", "number"), ("TableKind", "string"), ("TableName", "string"), ("Columns", "array"))
+            props = self.extract_props(frame_type, ("TableId", JsonTokenType.NUMBER), ("TableKind", JsonTokenType.STRING), ("TableName", JsonTokenType.STRING),
+                                       ("Columns", JsonTokenType.START_ARRAY))
             self.reader.skip_until_property_name("Rows")
-            props["Rows"] = self.row_iterator(props["Columns"])
+            props["Rows"] = list(self.row_iterator(props["Columns"]))
             return props
         elif frame_type == FrameType.DataSetCompletion:
-            res = self.extract_props(frame_type, ("HasErrors", "boolean"), ("Cancelled", "boolean"))
+            res = self.extract_props(frame_type, ("HasErrors", JsonTokenType.BOOLEAN), ("Cancelled", JsonTokenType.BOOLEAN))
             token = self.reader.skip_until_property_name_or_end_object("OneApiErrors")
-            if token.token_type != "end_map":
+            if token.token_type != JsonTokenType.END_MAP:
                 res["OneApiErrors"] = self.parse_array(skip_start=True)
             return res
 
     def row_iterator(self, columns):
-        self.reader.read_token_of_type("start_array")
+        self.reader.read_token_of_type(JsonTokenType.START_ARRAY)
         while True:
-            token = self.reader.read_token_of_type("start_array", "end_array")
-            if token.token_type == "end_array":
+            token = self.reader.read_token_of_type(JsonTokenType.START_ARRAY, JsonTokenType.END_ARRAY)
+            if token.token_type == JsonTokenType.END_ARRAY:
                 return
             row = {}
             for i in range(len(columns)):
                 token = self.reader.read_next_token_or_throw()
-                if token.token_type == "start_map":
+                if token.token_type == JsonTokenType.START_MAP:
                     row[columns[i]["ColumnName"]] = self.parse_object(skip_start=True)
-                elif token.token_type == "start_array":
+                elif token.token_type == JsonTokenType.START_ARRAY:
                     row[columns[i]["ColumnName"]] = self.parse_array(skip_start=True)
                 else:
                     row[columns[i]["ColumnName"]] = token.token_value
-            self.reader.read_token_of_type("end_array")
+            self.reader.read_token_of_type(JsonTokenType.END_ARRAY)
             yield row
 
     def parse_array(self, skip_start):
@@ -168,14 +192,15 @@ class ProgressiveDataSetEnumerator:
         arr = []
 
         while True:
-            token = self.reader.read_token_of_type("null", "boolean", "number", "string", "start_map", "start_array", "end_array")
+            token = self.reader.read_token_of_type(JsonTokenType.NULL, JsonTokenType.BOOLEAN, JsonTokenType.NUMBER, JsonTokenType.STRING,
+                                                   JsonTokenType.START_MAP, JsonTokenType.START_ARRAY, JsonTokenType.END_ARRAY)
 
-            if token.token_type == "end_array":
+            if token.token_type == JsonTokenType.END_ARRAY:
                 return arr
 
-            if token.token_type == "start_map":
+            if token.token_type == JsonTokenType.START_MAP:
                 arr.append(self.parse_object(skip_start=True))
-            elif token.token_type == "start_array":
+            elif token.token_type == JsonTokenType.START_ARRAY:
                 arr.append(self.parse_array(skip_start=True))
             else:
                 arr.append(token.token_value)
@@ -186,28 +211,26 @@ class ProgressiveDataSetEnumerator:
 
         obj = {}
         while True:
-            token_prop_name = self.reader.read_token_of_type("map_key", "end_map")
-            if token_prop_name.token_type == "end_map":
+            token_prop_name = self.reader.read_token_of_type(JsonTokenType.MAP_KEY, JsonTokenType.END_MAP)
+            if token_prop_name.token_type == JsonTokenType.END_MAP:
                 return obj
             prop_name = token_prop_name.token_value
 
-            token = self.reader.read_token_of_type("null", "boolean", "number", "string", "start_map", "start_array")
+            token = self.reader.read_token_of_type(JsonTokenType.NULL, JsonTokenType.BOOLEAN, JsonTokenType.NUMBER, JsonTokenType.STRING,
+                                                   JsonTokenType.START_MAP, JsonTokenType.START_ARRAY)
 
-            if token.token_type == "start_map":
+            if token.token_type == JsonTokenType.START_MAP:
                 obj[prop_name] = self.parse_object(skip_start=True)
-            elif token.token_type == "start_array":
+            elif token.token_type == JsonTokenType.START_ARRAY:
                 obj[prop_name] = self.parse_array(skip_start=True)
             else:
                 obj[prop_name] = token.token_value
 
-
-    def extract_props(self, frame_type, *props: Tuple[str, str]) -> Dict[str, Any]:
+    def extract_props(self, frame_type, *props: Tuple[str, JsonTokenType]) -> Dict[str, Any]:
         result = {"frame_type": frame_type}
         for (name, type) in props:
-            token = self.reader.skip_until_property_name(name)
-            if type == "object":
-                result[name] = self.parse_object(skip_start=False)
-            elif type == "array":
+            self.reader.skip_until_property_name(name)
+            if type == JsonTokenType.START_ARRAY:
                 result[name] = self.parse_array(skip_start=False)
             else:
                 result[name] = self.reader.read_token_of_type(type).token_value
@@ -228,7 +251,7 @@ class KustoJsonDataStreamReader:
 
         # "Version" : "X.Y -- Optional
         token = self.reader.skip_until_property_name_or_end_object("Version", "Tables", "error")
-        if token.token_type == "end_map":
+        if token.token_type == JsonTokenType.END_MAP:
             raise Exception("There is no table in the stream")
 
         if token.token_value == "Version":
--- azure-kusto-data/setup.py
+++ azure-kusto-data/setup.py
@@ -43,6 +43,6 @@ setup(
     namespace_packages=["azure"],
     keywords="kusto wrapper client library",
     packages=find_packages(exclude=["azure", "tests"]),
-    install_requires=["python-dateutil>=2.8.0", "requests>=2.13.0", "azure-identity~=1.5.0", "msal~=1.9.0"],
+    install_requires=["python-dateutil>=2.8.0", "requests>=2.13.0", "azure-identity~=1.5.0", "msal~=1.9.0", "ijson~=3.1.4"],
     extras_require={"pandas": ["pandas"], "aio": ["aiohttp~=3.4.4", "asgiref~=3.2.3"]},
 )
