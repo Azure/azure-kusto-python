fd991d648c7a90d8452a3f83e42543f00d824f77
Asaf Mahlev <asafmahlev@microsoft.com>
2021-07-11 14:33:40 +0300

    dynamic test

--- azure-kusto-data/tests/test_streaming_query.py
+++ azure-kusto-data/tests/test_streaming_query.py
@@ -45,6 +45,15 @@ class TestStreamingQuery(KustoClientTestsMixin):
                 if i["FrameType"] == FrameType.DataTable and i["TableKind"] == WellKnownDataSet.PrimaryResult.value:
                     self._assert_sanity_query_primary_results(i["Rows"])
 
+    def test_dynamic(self):
+        with self.open_json_file("dynamic.json") as f:
+            reader = ProgressiveDataSetEnumerator(JsonTokenReader(f))
+
+            for i in reader:
+                if i["FrameType"] == FrameType.DataTable and i["TableKind"] == WellKnownDataSet.PrimaryResult.value:
+                    row = next(i["Rows"])
+                    self._assert_dynamic_response(list(row.values()))
+
     @pytest.mark.asyncio
     async def test_sanity_async(self):
         with self.open_async_json_file("deft.json") as f:
@@ -54,3 +63,12 @@ class TestStreamingQuery(KustoClientTestsMixin):
                 if i["FrameType"] == FrameType.DataTable and i["TableKind"] == WellKnownDataSet.PrimaryResult.value:
                     rows = [x async for x in i["Rows"]]
                     self._assert_sanity_query_primary_results(rows)
+
+    @pytest.mark.asyncio
+    async def test_async_dynamic(self):
+        with self.open_async_json_file("dynamic.json") as f:
+            reader = AsyncProgressiveDataSetEnumerator(AsyncJsonTokenReader(f))
+            async for i in reader:
+                if i["FrameType"] == FrameType.DataTable and i["TableKind"] == WellKnownDataSet.PrimaryResult.value:
+                    row = await i["Rows"].__anext__()
+                    self._assert_dynamic_response(list(row.values()))
