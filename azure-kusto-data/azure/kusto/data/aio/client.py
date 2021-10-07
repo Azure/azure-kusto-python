b142f035282a1ff837d34352833486c699b74a6d
Asaf Mahlev <asafmahlev@microsoft.com>
2021-10-07 15:24:27 +0300

    raise

--- azure-kusto-data/azure/kusto/data/aio/client.py
+++ azure-kusto-data/azure/kusto/data/aio/client.py
@@ -112,7 +112,7 @@ class KustoClient(_KustoClientBase):
                     response_json = await response.json()
                 except Exception:
                     response_json = None
-                self._handle_http_error(e, endpoint, payload, response, response.status, response_json, response_text)
+                raise self._handle_http_error(e, endpoint, payload, response, response.status, response_json, response_text)
 
         async with response:
             response_json = None
@@ -124,6 +124,6 @@ class KustoClient(_KustoClientBase):
                     response_text = await response.text()
                 except Exception:
                     response_text = None
-                self._handle_http_error(e, endpoint, payload, response, response.status, response_json, response_text)
+                raise self._handle_http_error(e, endpoint, payload, response, response.status, response_json, response_text)
 
             return self._kusto_parse_by_endpoint(endpoint, response_json)
