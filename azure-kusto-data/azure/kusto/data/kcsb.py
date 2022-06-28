3a15e5eede5e2c0d7639298375e770e0bd6be4c1
enmoed <101040590+enmoed@users.noreply.github.com>
2022-06-28 11:22:55 +0300

    Added tests to check parsing correctness of proxy URLs (#401)
    * url fix for parsing
    
    * Update kcsb.py
    
    * Update kcsb.py
    
    * Update kcsb.py
    
    Co-authored-by: AsafMah <asafmahlev@microsoft.com>

--- azure-kusto-data/azure/kusto/data/kcsb.py
+++ azure-kusto-data/azure/kusto/data/kcsb.py
@@ -128,12 +128,13 @@ class KustoConnectionStringBuilder:
         for kvp_string in connection_string.split(";"):
             key, _, value = kvp_string.partition("=")
             keyword = self.ValidKeywords.parse(key)
+            value_stripped = value.strip()
             if keyword.is_str_type():
-                self[keyword] = value.strip()
-            if keyword.is_bool_type():
-                if value.strip() in ["True", "true"]:
+                self[keyword] = value_stripped.rstrip("/")
+            elif keyword.is_bool_type():
+                if value_stripped in ["True", "true"]:
                     self[keyword] = True
-                elif value.strip() in ["False", "false"]:
+                elif value_stripped in ["False", "false"]:
                     self[keyword] = False
                 else:
                     raise KeyError("Expected aad federated security to be bool. Recieved %s" % value)
