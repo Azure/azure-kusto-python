cbcffc6d85840371f60360382cf4d5f8bbbc2351
Yihezkel Schoenbrun <yischoen@microsoft.com>
2022-10-30 18:06:24 +0200

    Revert unintended change

--- test.bat
+++ test.bat
@@ -1,5 +1,6 @@
 cd %PROJECTS_HOME%\azure-kusto-python
-call pip uninstall azure-kusto-data azure-kusto-ingest -y
+call workon kusto
+call pip uninstall azure-kusto-data azure-kusto-ingest -y 
 call pip install ./azure-kusto-data[pandas] ./azure-kusto-ingest[pandas] 
 call pip install --force-reinstall azure-nspkg==2.0.0 
 call pytest
