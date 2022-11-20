95000fc5ed4b64771c058b649d8d6372a684a5c9
Creste <creste@users.noreply.github.com>
2022-11-20 15:15:58 -0500

    Switched to pytest and added assert to check socket_options.

--- azure-kusto-data/tests/test_http_adapter_with_socket_options.py
+++ azure-kusto-data/tests/test_http_adapter_with_socket_options.py
@@ -1,13 +1,14 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License
 import pickle
-import unittest
 import socket
 
 from azure.kusto.data.client import HTTPAdapterWithSocketOptions
 
-class HTTPAdapterWithSocketOptionsTests(unittest.TestCase):
-    def test_pickle(self):
-        adapter = HTTPAdapterWithSocketOptions(socket_options=[(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)])
-        pickled = pickle.dumps(adapter)
-        pickle.loads(pickled)
+
+def test_pickle():
+    socket_options = [(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)]
+    original = HTTPAdapterWithSocketOptions(socket_options=socket_options)
+    unpickled = pickle.loads(pickle.dumps(original))
+    assert unpickled.socket_options == socket_options
+
