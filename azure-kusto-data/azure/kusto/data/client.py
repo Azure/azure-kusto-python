e2f66bf271e7f56866438198d2c50e2d6a1f0435
Creste <creste@users.noreply.github.com>
2022-11-20 14:39:10 -0500

    Removed pool_maxsize and max_retries because they are not used.

--- azure-kusto-data/azure/kusto/data/client.py
+++ azure-kusto-data/azure/kusto/data/client.py
@@ -23,8 +23,6 @@ if TYPE_CHECKING:
 class HTTPAdapterWithSocketOptions(requests.adapters.HTTPAdapter):
     def __init__(self, *args, **kwargs):
         self.socket_options = kwargs.pop("socket_options", None)
-        self.pool_maxsize = kwargs.get("pool_maxsize", None)
-        self.max_retries = kwargs.get("max_retries", None)
         super(HTTPAdapterWithSocketOptions, self).__init__(*args, **kwargs)
 
     def __getstate__(self):
