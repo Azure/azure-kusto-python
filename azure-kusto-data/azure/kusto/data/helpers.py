658d9451c038916aa4f2e04cf0c6fe4db6c1bf53
AsafMah <asafmahlev@microsoft.com>
2023-01-22 14:12:56 +0200

    Use proper np types instead of strings

--- azure-kusto-data/azure/kusto/data/helpers.py
+++ azure-kusto-data/azure/kusto/data/helpers.py
@@ -51,14 +51,14 @@ def dataframe_from_result_table(table: "Union[KustoResultTable, KustoStreamingRe
     # fix types
     for col in table.columns:
         if col.column_type == "bool":
-            frame[col.column_name] = frame[col.column_name].astype("boolean" if nullable_bools else bool)
+            frame[col.column_name] = frame[col.column_name].astype(np.bool if nullable_bools else bool)
         elif col.column_type == "int":
-            frame[col.column_name] = frame[col.column_name].astype("Int32")
+            frame[col.column_name] = frame[col.column_name].astype(np.int32)
         elif col.column_type == "long":
-            frame[col.column_name] = frame[col.column_name].astype("Int64")
+            frame[col.column_name] = frame[col.column_name].astype(np.int64)
         elif col.column_type == "real" or col.column_type == "decimal":
             frame[col.column_name] = frame[col.column_name].replace("NaN", np.NaN).replace("Infinity", np.PINF).replace("-Infinity", np.NINF)
-            frame[col.column_name] = pd.to_numeric(frame[col.column_name], errors="coerce").astype("Float64")
+            frame[col.column_name] = pd.to_numeric(frame[col.column_name], errors="coerce").astype(np.float64)
         elif col.column_type == "datetime":
             frame[col.column_name] = pd.to_datetime(frame[col.column_name], errors="coerce")
         elif col.column_type == "timespan":
