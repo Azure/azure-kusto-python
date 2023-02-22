939a36377315c5c670dea9bea5736aea31e4c24d
AsafMah <asafmahlev@microsoft.com>
2023-02-22 10:29:59 +0200

    Fixed resource parsing to be inline with other SDKs (#458)
    * Fixed resource parsing to be inline with other SDKs
    
    * Fixed resource parsing to be inline with other SDKs

--- azure-kusto-ingest/tests/test_connection_string.py
+++ azure-kusto-ingest/tests/test_connection_string.py
@@ -1,38 +1,43 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License
-import unittest
+
 from azure.kusto.ingest._resource_manager import _ResourceUri
 
 
-class ResourceUriTests(unittest.TestCase):
-    """Tests class connection_string."""
+def test_blob_uri():
+    """Tests parsing blob uris."""
+    storage_name = "storageaccountname"
+    container_name = "containername"
+    endpoint_suffix = "core.windows.net"
+    container_sas = "somesas"
 
-    def test_blob_uri(self):
-        """Tests parsing blob uris."""
-        storage_name = "storageaccountname"
-        container_name = "containername"
-        endpoint_suffix = "core.windows.net"
-        container_sas = "somesas"
+    uri = "https://{}.blob.{}/{}?{}".format(storage_name, endpoint_suffix, container_name, container_sas)
+    connection_string = _ResourceUri(uri)
+    assert connection_string.account_uri == "https://storageaccountname.blob.core.windows.net/?somesas"
+    assert connection_string.object_name == container_name
 
-        uri = "https://{}.blob.{}/{}?{}".format(storage_name, endpoint_suffix, container_name, container_sas)
-        connection_string = _ResourceUri.parse(uri)
-        assert connection_string.storage_account_name == storage_name
-        assert connection_string.object_type == "blob"
-        assert connection_string.endpoint_suffix == endpoint_suffix
-        assert connection_string.sas == container_sas
-        assert connection_string.object_name == container_name
 
-    def test_queue_uri(self):
-        """Tests parsing queues uris."""
-        storage_name = "storageaccountname"
-        queue_name = "queuename"
-        endpoint_suffix = "core.windows.net"
-        queue_sas = "somesas"
+def test_queue_uri():
+    """Tests parsing queues uris."""
+    storage_name = "storageaccountname"
+    queue_name = "queuename"
+    endpoint_suffix = "core.windows.net"
+    queue_sas = "somesas"
 
-        uri = "https://{}.queue.{}/{}?{}".format(storage_name, endpoint_suffix, queue_name, queue_sas)
-        connection_string = _ResourceUri.parse(uri)
-        assert connection_string.storage_account_name == storage_name
-        assert connection_string.object_type == "queue"
-        assert connection_string.endpoint_suffix == endpoint_suffix
-        assert connection_string.sas == queue_sas
-        assert connection_string.object_name == queue_name
+    uri = "https://{}.queue.{}/{}?{}".format(storage_name, endpoint_suffix, queue_name, queue_sas)
+    connection_string = _ResourceUri(uri)
+    assert connection_string.account_uri == "https://storageaccountname.queue.core.windows.net/?somesas"
+    assert connection_string.object_name == queue_name
+
+
+def test_gov_cloud_uri():
+    """Tests parsing queues uris."""
+    storage_name = "storageaccountname"
+    queue_name = "queuename"
+    endpoint_suffix = "core.eaglex.ic.gov"
+    queue_sas = "somesas"
+
+    uri = "https://{}.queue.{}/{}?{}".format(storage_name, endpoint_suffix, queue_name, queue_sas)
+    connection_string = _ResourceUri(uri)
+    assert connection_string.account_uri == "https://storageaccountname.queue.core.eaglex.ic.gov/?somesas"
+    assert connection_string.object_name == queue_name
