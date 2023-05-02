f9bbdd30043cfeaebf5af253315e5ee93f204f85
enmoed <101040590+enmoed@users.noreply.github.com>
2023-05-02 13:47:10 +0300

    added tests

--- azure-kusto-data/tests/test_telemetry.py
+++ azure-kusto-data/tests/test_telemetry.py
@@ -19,16 +19,61 @@ class TestTelemetry:
         SpanAttributes.add_attributes(tracing_attributes={"foo": "bar"})
         return num + 1
 
-    # @staticmethod
-    # def test_call_func_tracing():
-    #     res = Span._call_func_tracing(TestTelemetry.plus_one, 1, name_of_span="plus_one")
-    #     assert res == 2
-    #
-    # @staticmethod
-    # @pytest.mark.asyncio
-    # async def test_call_func_tracing_async():
-    #     res = Span._call_func_tracing_async(TestTelemetry.plus_one_async, 1, name_of_span="plus_one")
-    #     assert await res == 2
+    @staticmethod
+    def test_run_valid_invoker():
+        # Happy path test for run method with valid invoker function and name of span
+        def invoker():
+            return "Hello World"
+
+        span = Span.run(invoker, "test_span")
+        assert span is not None
+
+    @staticmethod
+    def test_run_async_valid_invoker():
+        # Happy path test for run_async method with valid invoker function and name of span
+        async def invoker():
+            return "Hello World"
+
+        span = Span.run_async(invoker, "test_span")
+        assert span is not None
+
+    @staticmethod
+    def test_run_none_invoker():
+        # Edge case test for run method with None invoker function
+        with pytest.raises(TypeError):
+            Span.run(None, "test_span")
+
+    @staticmethod
+    def test_run_async_none_invoker():
+        # Edge case test for run_async method with None invoker function
+        with pytest.raises(TypeError):
+            Span.run_async(None, "test_span")
+
+    @staticmethod
+    def test_run_sync_behavior():
+        # General behavior test for run method running the span synchronously
+        def invoker():
+            return "Hello World"
+
+        span = Span.run(invoker, "test_span")
+        assert span == "Hello World"
+
+    @staticmethod
+    async def test_run_async_behavior():
+        # General behavior test for run_async method running the span asynchronously
+        async def invoker():
+            return "Hello World"
+
+        span = await Span.run_async(invoker, "test_span")
+        assert span == "Hello World"
+
+    @staticmethod
+    def test_tracing_attributes_parameter():
+        def invoker():
+            return "Hello World"
+        tracing_attributes = {"key": "value"}
+        result = Span.run(invoker, tracing_attributes=tracing_attributes)
+        assert result == "Hello World"
 
     @staticmethod
     def test_get_client_request_properties_attributes():
@@ -60,7 +105,8 @@ class TestTelemetry:
 
     @staticmethod
     def test_create_ingest_attributes():
-        attributes = SpanAttributes.create_streaming_ingest_attributes("cluster_test", "database_test", "table", ClientRequestProperties())
+        attributes = SpanAttributes.create_streaming_ingest_attributes("cluster_test", "database_test", "table",
+                                                                       ClientRequestProperties())
         keynames = {"kusto_cluster", "database", "table", "client_request_id"}
         assert isinstance(attributes, dict)
         for key, val in attributes.items():
@@ -81,4 +127,5 @@ class TestTelemetry:
         assert attributes == {"component": "http", "http.method": "method_test", "http.url": "url_test"}
         headers = {"User-Agent": "user_agent_test"}
         attributes = SpanAttributes.create_http_attributes("method_test", "url_test", headers)
-        assert attributes == {"component": "http", "http.method": "method_test", "http.url": "url_test", "http.user_agent": "user_agent_test"}
+        assert attributes == {"component": "http", "http.method": "method_test", "http.url": "url_test",
+                              "http.user_agent": "user_agent_test"}
