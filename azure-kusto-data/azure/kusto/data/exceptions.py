ccbf22265b8b5652210efeafdd518b11d7ee4583
Shira Har-Nes <shiraharnes@microsoft.com>
2023-07-11 15:50:21 +0300

    Changed place for KustoNetworkError

--- azure-kusto-data/azure/kusto/data/exceptions.py
+++ azure-kusto-data/azure/kusto/data/exceptions.py
@@ -111,11 +111,6 @@ class KustoMultiApiError(KustoServiceError):
                     parsed_errors.append(OneApiError.from_dict(error_dict))
         return parsed_errors
 
-class KustoNetworkError(KustoServiceError):
-    """ Raised when a Kusto client fails to connect to network."""
-    def __init__(self):
-        super().__init__("Failed to process network request")
-
 class KustoApiError(KustoServiceError):
     """
     Represents a standard API error from kusto. Use `get_api_error()` to retrieve more details.
@@ -128,6 +123,10 @@ class KustoApiError(KustoServiceError):
     def get_api_error(self) -> OneApiError:
         return self.error
 
+class KustoNetworkError(KustoServiceError):
+    """ Raised when a Kusto client fails to connect to network."""
+    def __init__(self):
+        super().__init__("Failed to process network request")
 
 class KustoClientError(KustoError):
     """Raised when a Kusto client is unable to send or complete a request."""
