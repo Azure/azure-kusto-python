f63092624e2daa9598ec1fb504017c9f12742caf
Maya Marom <t-mayamarom@microsoft.com>
2024-07-01 12:14:18 +0300

    git ignore out production files

---
+++ out/production/azure-kusto-data/kusto/data/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+from ._version import VERSION as __version__
+from .client import KustoClient
+from .client_request_properties import ClientRequestProperties
+from .kcsb import KustoConnectionStringBuilder
+from .data_format import DataFormat
