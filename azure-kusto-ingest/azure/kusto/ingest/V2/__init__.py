7813f9db41f6b110b5476a2c403b07b4f71e5433
Maya Marom <t-mayamarom@microsoft.com>
2024-07-01 13:17:33 +0300

    added init file for V2

---
+++ azure-kusto-ingest/azure/kusto/ingest/V2/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License
+from .blob_source import BlobSource
+from .compression_type import CompressionType
+from .ingestion_source import IngestionSource
+from .local_source import LocalSource, FileSource, StreamSource
+from .kusto_storage_uploader import KustoStorageUploader
