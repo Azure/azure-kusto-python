7271aaf5c66b16028333cbfa13835d297ef0c7dc
asafmahlev <asafmahlev@microsoft.com>
2024-10-28 11:34:49 +0200

    Add py-typed markers.

--- azure-kusto-data/setup.py
+++ azure-kusto-data/setup.py
@@ -45,7 +45,7 @@ setup(
     namespace_packages=["azure"],
     keywords="kusto wrapper client library",
     packages=find_packages(exclude=["azure", "*tests*", "*tests.*"]),
-    package_data={"": ["wellKnownKustoEndpoints.json"]},
+    package_data={"": ["wellKnownKustoEndpoints.json", "py.typed"]},
     include_package_data=True,
     install_requires=["python-dateutil>=2.8.0", "requests>=2.13.0", "azure-identity>=1.5.0,<2", "msal>=1.9.0,<2", "ijson~=3.1", "azure-core>=1.11.0,<2"],
     extras_require={"pandas": ["pandas"], "aio": ["aiohttp>=3.8.0,<4", "asgiref>=3.2.3,<4"]},
