82d18b557ea126d702a8afc59663cf0271dc6d5b
AsafMah <asafmahlev@microsoft.com>
2024-11-03 15:04:30 +0200

    Add py-typed markers. (#555)

--- azure-kusto-ingest/setup.py
+++ azure-kusto-ingest/setup.py
@@ -43,6 +43,7 @@ setup(
         "License :: OSI Approved :: MIT License",
     ],
     packages=find_packages(exclude=["azure", "*tests*", "*tests.*"]),
+    package_data={"": ["py.typed"]},
     install_requires=["azure-kusto-data=={}".format(VERSION), "azure-storage-blob>=12,<13", "azure-storage-queue>=12,<13", "tenacity>=8.0.0"],
     extras_require={"pandas": ["pandas"], "aio": []},
 )
