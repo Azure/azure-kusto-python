60fc0f2c31ba1238574606b1dd27c209a2052b7a
Asaf Mahlev <asafmahlev@microsoft.com>
2025-02-25 13:43:43 +0200

    New logic

--- azure-kusto-ingest/azure/kusto/ingest/base_ingest_client.py
+++ azure-kusto-ingest/azure/kusto/ingest/base_ingest_client.py
@@ -121,8 +121,9 @@ class BaseIngestClient(metaclass=ABCMeta):
         is_json = True
 
         # If we are given CSV mapping, or the mapping format is explicitly set to CSV, we should use CSV
-        if not data_format and ingestion_properties is not None and (ingestion_properties.ingestion_mapping_type == DataFormat.CSV):
-            is_json = False
+        if not data_format:
+            if ingestion_properties is not None and (ingestion_properties.ingestion_mapping_type == DataFormat.CSV):
+                is_json = False
         elif data_format == DataFormat.CSV:
             is_json = False
         elif data_format != DataFormat.JSON:
