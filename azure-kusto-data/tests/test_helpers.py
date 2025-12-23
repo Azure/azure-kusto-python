ddf6af42b0fe73036605fccea0b77ea3c7b76bb6
AsafMah <asafmahlev@microsoft.com>
2025-12-23 09:57:18 +0200

    Update azure-kusto-data/tests/test_helpers.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- azure-kusto-data/tests/test_helpers.py
+++ azure-kusto-data/tests/test_helpers.py
@@ -163,4 +163,3 @@ def test_parse_datetime():
     result_actual = parse_datetime(df_actual, "mixed")
     assert result_actual[0] == pandas.Timestamp(year=2023, month=12, day=12, hour=1, minute=59, second=59, microsecond=352000, tz="UTC")
     assert result_actual[1] == pandas.Timestamp(year=2023, month=12, day=12, hour=1, minute=54, second=44, tz="UTC")
-
